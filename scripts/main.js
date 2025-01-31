6d5eb54cec682f68bb29438e386b65324aada3a5
Daysi Schmadtke <daysischmadtke@gmail.com>
2025-01-30 21:26:24 -0500

    first part of exercise

--- scripts/main.js
+++ scripts/main.js
@@ -7,40 +7,50 @@ import { renderToDom } from "../utils/renderToDom.js";
 // .forEach()
 const renderCards = (array) => {
   let refStuff = "<h1 class='text-white'>Cards Go Here!</h1>";
+
+  array.forEach((item) => {
+    refStuff += card(item);
+  });
+
   renderToDom("#cards", refStuff);
-}
+};
 
 // UPDATE/ADD ITEMS TO CART
 // .findIndex() & (.includes() - string method)
 const toggleCart = (event) => {
   if (event.target.id.includes("fav-btn")) {
-   console.log('Clicked Fav btn')
+    console.log("Clicked Fav btn");
   }
-}
+};
 
 // SEARCH
 // .filter()
 const search = (event) => {
   const eventLC = event.target.value.toLowerCase();
-  console.log(eventLC)
-}
+  console.log(eventLC);
+};
 
 // BUTTON FILTER
 // .filter() & .reduce() &.sort() - chaining
 const buttonFilter = (event) => {
-  if(event.target.id.includes('free')) {
-    console.log('FREE')
+  if (event.target.id.includes("free")) {
+    const free = referenceList.filter((item) => item.price <= 0);
+    renderCards(free);
   }
-  if(event.target.id.includes('cartFilter')) {
-    console.log('cartFilter')
+  if (event.target.id.includes("cartFilter")) {
+    const wishList = referenceList.filter((item) => item.inCart);
+    renderCards(wishList);
   }
-  if(event.target.id.includes('books')) {
-    console.log('books!')
+  if (event.target.id.includes("books")) {
+    const books = referenceList.filter(
+      (item) => item.type.toLowerCase() === "book"
+    );
+    renderCards(books);
   }
-  if(event.target.id.includes('clearFilter')) {
-    console.log('clearFilter')
+  if (event.target.id.includes("clearFilter")) {
+    renderCards(referenceList);
   }
-  if(event.target.id.includes('productList')) {
+  if (event.target.id.includes("productList")) {
     let table = `<table class="table table-dark table-striped" style="width: 600px">
     <thead>
       <tr>
@@ -51,46 +61,48 @@ const buttonFilter = (event) => {
     </thead>
     <tbody>
     `;
-    
-    productList().forEach(item => {
+
+    productList().forEach((item) => {
       table += tableRow(item);
     });
 
-    table += `</tbody></table>`
+    table += `</tbody></table>`;
 
-    renderToDom('#cards', table);
+    renderToDom("#cards", table);
   }
-  
-}
+};
 
 // CALCULATE CART TOTAL
 // .reduce() & .some()
 const cartTotal = () => {
-  const total = 0
+  const total = 0;
   document.querySelector("#cartTotal").innerHTML = total.toFixed(2);
-}
+};
 
 // RESHAPE DATA TO RENDER TO DOM
 // .map()
 const productList = () => {
-  return [{ title: "SAMPLE TITLE", price: 45.00, type: "SAMPLE TYPE" }]
-}
-
+  return referenceList.map((item) => ({
+    title: item.title,
+    price: item.price,
+    type: item.type,
+  }));
+};
 
 const startApp = () => {
   // PUT ALL CARDS ON THE DOM
-  renderCards(referenceList)
+  renderCards(referenceList);
 
   // PUT CART TOTAL ON DOM
   cartTotal();
 
   // SELECT THE CARD DIV
-  document.querySelector('#cards').addEventListener('click', toggleCart);
+  document.querySelector("#cards").addEventListener("click", toggleCart);
 
   // SELECT THE SEARCH INPUT
-  document.querySelector('#searchInput').addEventListener('keyup', search)
+  document.querySelector("#searchInput").addEventListener("keyup", search);
 
   // SELECT BUTTON ROW DIV
-  document.querySelector('#btnRow').addEventListener('click', buttonFilter);
-}
+  document.querySelector("#btnRow").addEventListener("click", buttonFilter);
+};
 startApp();
